b030856b98af140f759c8ec38d00d9df84898fd8
nghaosiong98 <nghaosiong98@gmail.com>
2020-10-24 20:32:20 +0800

    First commit
    - added React Navigation 5
    - created navigation boilerplate

---
+++ src/navigations/BottomTabNavigator.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./HomeScreen";
+import SettingScreen from "./SettingScreen";
+import ProfileScreen from "./ProfileScreen";
+
+const BottomTabNavigator = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Setting" component={SettingScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
