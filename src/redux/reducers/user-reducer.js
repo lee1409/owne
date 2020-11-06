6704829573ad3fc14ecef3d7fb6806b8f4eea9f4
nghaosiong98 <nghaosiong98@gmail.com>
2020-11-06 20:22:31 +0800

    Added eslint airbnb and redux saga

---
+++ src/redux/reducers/user-reducer.js
@@ -0,0 +1,19 @@
+import {
+  SET_USER_INFO
+} from "../actions/user-action";
+
+const initialState = {
+  userToken: '',
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case SET_USER_INFO:
+      return {
+        ...state,
+        ...action.userData,
+      };
+    default:
+      return state;
+  }
+};
