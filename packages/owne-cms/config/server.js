ff33faac0008bea33de9d8cb8632be220fb5db79
lee1409 <leerun1409@gmail.com>
2021-06-16 13:37:06 +0800

    added cms

---
+++ packages/owne-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '508b7920ecabb278571e615cf7f666c2'),
+    },
+  },
+});
