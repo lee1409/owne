97bc8d18e5b76b0590e7b272ef0626e37bb7271f
nghaosiong98 <nghaosiong98@gmail.com>
2021-06-26 23:34:33 +0800

    feat: added postgres connection
    - added pg
    - updated database configuration

--- packages/owne-cms/config/database.js
+++ packages/owne-cms/config/database.js
@@ -4,8 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOSTNAME'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: env('DATABASE_SSL', false),
       },
       options: {
         useNullAsDefault: true,
