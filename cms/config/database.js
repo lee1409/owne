ca8f00421dabb461131e46917b163a84d779d8c0
lee1409 <leerun1409@gmail.com>
2021-06-27 07:07:01 +0800

    move file structure, rename and prevent hoisitng for app

---
+++ cms/config/database.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => {
+  const settings =
+    env("NODE_ENV") === "development"
+      ? ({ client: "sqlite", filename: env("DATABASE_FILENAME", ".tmp/data.db") })
+      : ({
+          client: "postgres",
+          host: env("DATABASE_HOSTNAME"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME"),
+          username: env("DATABASE_USERNAME"),
+          password: env("DATABASE_PASSWORD"),
+          schema: env("DATABASE_SCHEMA", "public"),
+          ssl: env("DATABASE_SSL", false),
+        })
+
+  const obj = {
+    defaultConnection: "default",
+    connections: {
+
+      default: {
+        connector: "bookshelf",
+        settings,
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  }
+  return obj
+}
