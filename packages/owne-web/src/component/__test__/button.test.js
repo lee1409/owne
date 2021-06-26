a76d9806caa305b2686251208e60763b1de62493
lee1409 <leerun1409@gmail.com>
2021-06-26 21:14:45 +0800

    added button

---
+++ packages/owne-web/src/component/__test__/button.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import {ShareButton} from "../button";
+
+test("share button", () => {
+  let button = render(<ShareButton></ShareButton>)
+  expect(button).toMatchSnapshot("shareButton")
+})
