9fbe30ab679ee64ed86ffa2917aeb5b5771a4988
nghaosiong98 <nghaosiong98@gmail.com>
2021-07-14 21:25:13 +0800

    feat: implemented TS to codebase

---
+++ web/src/component/__test__/typograhy.test.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import { H1 } from "../typography"
+
+test("H1", () => {
+  let h1 = render(<H1>Testing</H1>)
+  expect(h1).toMatchSnapshot("h1")
+})
