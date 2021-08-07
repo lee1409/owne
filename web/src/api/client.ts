581fdef2f485252842094c0e8703eb8319eefbb2
lee1409 <leerun1409@gmail.com>
2021-08-07 22:55:56 +0800

    added client

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
+if (!process.env.REACT_APP_PUBLIC_URL) {
+  throw new Error("Please define graphql URL!")
+}
+
 const client = new ApolloClient({
-  uri: "",
+  uri: process.env.REACT_APP_PUBLIC_URL,
   cache: new InMemoryCache(),
 })
 
