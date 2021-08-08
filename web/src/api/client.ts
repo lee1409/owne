b238d3db88fa0a0202c9ea474aeaa093a9f59399
lee1409 <leerun1409@gmail.com>
2021-08-08 10:34:19 +0800

    connect with dev tools

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -7,6 +7,7 @@ if (!process.env.REACT_APP_PUBLIC_URL) {
 const client = new ApolloClient({
   uri: process.env.REACT_APP_PUBLIC_URL,
   cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV === "development" ? true : false,
 })
 
 export default client
