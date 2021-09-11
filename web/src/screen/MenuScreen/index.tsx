18da8d4e9f7ab11fb47034b9921419eee6f7a8d2
lee1409 <leerun1409@gmail.com>
2021-09-11 17:29:29 +0800

    create a draft menu screen for temporary

---
+++ web/src/screen/MenuScreen/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MenuScreen() {
+  const [state, setState] = React.useState<string | null>("loading");
+  // todo: request location to get the current location
+
+  if (state === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>Try to activate GPS for auto finding or search the restaurant</div>
+  )
+}
+
+export default MenuScreen
