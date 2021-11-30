2477eebfdaa50b7db2a8de4a4609016988f088ab
lee1409 <leerun1409@gmail.com>
2021-11-30 10:22:06 +0800

    feat: added jwt env

--- cms/scripts/build-app-yaml.js
+++ cms/scripts/build-app-yaml.js
@@ -30,6 +30,7 @@ console.log(
       DATABASE_NAME: process.env.DATABASE_NAME,
       DATABASE_USERNAME: process.env.DATABASE_USERNAME,
       DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+      JWT_SECRET: process.env.JWT_SECRET,
     },
   })
 );
