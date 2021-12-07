2b652a09f966e22f859a3ce306cb4da3b9a77653
lee1409 <leerun1409@gmail.com>
2021-12-07 14:53:23 +0800

    fix(web): set home path exact path

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -8,15 +8,17 @@ const MenuDetailScreen = lazy(() => import("../screen/MenuDetail"))
 const FoodScreen = lazy(() => import("../screen/Food"))
 const Home = lazy(() => import("../screen/Home"))
 const Page404 = lazy(() => import("../screen/Page404"))
+const LoginScreen = lazy(() => import("../screen/Login"))
 
 const Router = () => (
   <BrowserRouter basename={ROUTE_NAME.HOME}>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
+        <Route exact path={ROUTE_NAME.HOME} component={Home} />
         <Route exact path={ROUTE_NAME.MENU} component={MenuScreen} />
         <Route path={ROUTE_NAME.MENU_DETAIL} component={MenuDetailScreen} />
         <Route path={ROUTE_NAME.FOOD} component={FoodScreen} />
-        <Route path={ROUTE_NAME.HOME} component={Home} />
+
         <Route path={ROUTE_NAME.PAGE_404} component={Page404} />
       </Switch>
     </Suspense>
