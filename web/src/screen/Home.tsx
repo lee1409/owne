c4b1ddf6c4cdb37ccd03535b605c8e507499c71a
lee1409 <leerun1409@gmail.com>
2021-12-07 14:53:55 +0800

    feat: add a construction image

--- web/src/screen/Home.tsx
+++ web/src/screen/Home.tsx
@@ -1,8 +1,25 @@
+import { Container } from "@material-ui/core"
 import Layout from "../components/Layout"
+import UnderConstruction from "../assets/under-construction.svg"
 
 const Explore = () => (
   <Layout>
-    <>This is the main page. Wait till the data is populated</>
+    <Container
+      style={{
+        display: "grid",
+        placeItems: "center",
+        width: "100wh",
+        height: "20vh",
+      }}
+    >
+      <img
+        src={UnderConstruction}
+        width={200}
+        height={200}
+        alt="Under Construction"
+      />
+      This is the main page. Wait till the data is populated
+    </Container>
   </Layout>
 )
 
